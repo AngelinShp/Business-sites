af63e32417500a02bc8ed0218075a29d09fbaf5d
AngelinShp <angelinashpspb@gmail.com>
2023-12-01 15:41:05 +0300

    2_JS_Циклы

---
+++ Module 2/2_JS_Циклы/switch_begin.js
@@ -0,0 +1,14 @@
+var a = 2 + 2;
+switch (a) {
+  case 3:
+    alert('Очень мало.');
+    break;
+  case 4:
+    alert('Всё в норме!');
+    break;
+  case 5:
+    alert('Слишком много.');
+    break;
+  default:
+    alert('Неизвестное значение.');
+}
