65704a0d8e81c500c2129474293dd8345999fd05
AngelinShp <angelinashpspb@gmail.com>
2024-02-23 16:46:15 +0300

    PHP_begin

--- Module 3/4_PHP/helloworld.php
+++ Module 3/4_PHP/helloworld.php
@@ -1,4 +1,7 @@
-/* https://drive.google.com/file/d/1B4vbRI4nFwp3vn15pMK3Ut1d-QcZ5xdv/view */
+/* https://drive.google.com/file/d/1B4vbRI4nFwp3vn15pMK3Ut1d-QcZ5xdv/view
+https://ospanel.io/docs/
+*/
+
 <?php
 	echo 'Hello, world!';
 	print 'Hello, world!';
@@ -12,5 +15,6 @@
 
 <h1>Пример встраивания PHP в HTML</h1>
 <p><?php print('Hello, world!');?> </p>
+
 
 <p>2 + 2 = <?php echo 2 + 2; ?></p>
