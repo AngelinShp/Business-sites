aac863ca854eb160c89abd28ba034b3b5ddaa7bc
AngelinShp <angelinashpspb@gmail.com>
2024-02-23 15:56:42 +0300

    PHP_begin

---
+++ Module 3/4_PHP/helloworld.php
@@ -0,0 +1,15 @@
+<?php
+	echo 'Hello, world!';
+	print 'Hello, world!';
+?>
+<?= 'Hello, world'
+	/* короткий echo-тег, который является сокращением для
+	более многословного <?php echo ?> */
+?>
+
+<? echo 'Hello, world!'; // сокращённая версия   ?>
+
+<h1>Пример встраивания PHP в HTML</h1>
+<p><?php print('Hello, world!');?> </p>
+
+<p>2 + 2 = <?php echo 2 + 2; ?></p>
